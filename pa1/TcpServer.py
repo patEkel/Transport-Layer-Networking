4d7f46760dd864a5ae6d7d5e31ece57630712468
Patrick Ekel <patrick.ekel@utah.edu>
2018-01-19 18:55:51 -0700

    friday night

--- pa1/TcpServer.py
+++ pa1/TcpServer.py
@@ -1,4 +1,5 @@
 import socket
+
 serverPort = 12000
 serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 try:
@@ -7,13 +8,17 @@ except Exception:
 	print('in catch')
 serverSocket.listen(1)
 print('The Server is ready to receive')
+
 while True:
 	connectionSocket, addr = serverSocket.accept()
 	sentence = connectionSocket.recv(1024).decode()
-	capitalizedSentence = sentence.upper()
-	print(capitalizedSentence)
-	connectionSocket.send(capitalizedSentence.encode())
+	clientRequest = sentence.split(' ')
+	# do check for 3 itms, first get last something specific
+
+	endConnectionSocket, endAddr = serverSocket.accept() #port should be 80
+	endSentence = endConnectionSocket.recv(1024).decode()
+	
+	#connectionSocket.send(capitalizedSentence.encode())
+	connectionSocket.send(clientRequest[0].encode())
 	connectionSocket.close()
-
 	# if empy string break loop
-
