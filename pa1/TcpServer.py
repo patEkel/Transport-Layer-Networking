1bb2774c4df8a09fa3cef0451520b23697ca4878
ekel <ekel@lab1-1.eng.utah.edu>
2018-01-26 15:52:34 -0700

    clean up threading

--- pa1/TcpServer.py
+++ pa1/TcpServer.py
@@ -5,17 +5,42 @@ import threading
 import queue
 import urllib.parse
 
-
 def threaded(connection):
-	print("IN THREAD\n")
-	connection.send(str.encode("rickkky\n"))
-	while True:
-		data = connection.recv(2048)
-		reply = "Server says: " + data.decode()
-		if not data: # no more data
-			break
-		connection.sendall(str.encode(reply))	
-
+	print("IN THREAD\n")	
+	sentence = connection.recv(1024).decode()
+	clientInput =""
+	# build request from user, making sure entire request is received
+	while '\r\n' != sentence:
+		clientInput += sentence
+		sentence = connection.recv(1024).decode()
+	clientRequest = clientInput.split(' ')
+	# make sure request is a get request
+	if 'GET' not in clientRequest:
+		errorMessage = 'HTTP/1.0 501 Not Implemented\r\nerror: only GET requests are supported\r\n\r\n'
+		connection.send(errorMessage.encode())
+		connection.close()
+		return
+		
+	proxySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	if (len(clientRequest) == 3):
+		proxySocket.connect((urllib.parse.urlparse(clientRequest[1]).netloc, 80))
+		proxyRequest ='GET ' + urllib.parse.urlparse(clientRequest[1]).path + ' HTTP/1.0\r\nHost: ' + urllib.parse.urlparse(clientRequest[1]).netloc +'\r\n' +'Connection: close' + '\r\n\r\n'
+	else:
+		newstr = urllib.parse.urlparse(clientRequest[3]).path.replace('\r\n', '')
+		proxySocket.connect((newstr, 80))
+		proxyRequest ='GET ' + urllib.parse.urlparse(clientRequest[1]).path + ' HTTP/1.0\r\nHost: ' + newstr +'\r\n' +'Connection: close' + '\r\n\r\n'
+	
+	# forward request to end server, get response back
+	proxySocket.send(proxyRequest.encode())
+	responseMessage = proxySocket.recv(1024)
+	connection.send(responseMessage)
+	while '\r\n' != responseMessage:
+		responseMessage = proxySocket.recv(1024)
+		connection.send(responseMessage)
+	proxySocket.close()
+	# send response back to client
+	connection.send(responseMessage)
+	connection.close()
 
 if (len(sys.argv) != 2):
 	print("A valid port must be provided.")
@@ -37,22 +62,6 @@ while True:
 	connectionSocket, addr = serverSocket.accept()
 	print("IN MAIN\n")
 	threading.Thread(target = threaded, args = (connectionSocket, )).start()
-
-	# sentence = connectionSocket.recv(1024).decode()
-	# clientInput =""
-	# # build request from user, making sure entire request is received
-	# while '\r\n' != sentence:
-	# 	clientInput += sentence
-	# 	sentence = connectionSocket.recv(1024).decode()
-	# clientRequest = clientInput.split(' ')
-	# # make sure request is a get request
-	# if 'GET' not in clientRequest:
-	# 	errorMessage = 'HTTP/1.0 501 Not Implemented\r\nerror: only GET requests are supported\r\n\r\n'
-	# 	connectionSocket.send(errorMessage.encode())
-	# 	connectionSocket.close()
-	# 	exit()
-	# set up connection with remote server		
-	
 	###
 	#print("client request is: ", clientRequest, "\nand parsed client request is: ", urllib.parse.urlparse(clientInput))
 	#print("scheme is: ", urllib.parse.urlparse(clientInput).scheme)
@@ -65,25 +74,6 @@ while True:
 	#print(urllib.parse.urlparse(clientRequest[1]).netloc)
 	#print("==============================================\n")
 	###
-	
-	# proxySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	# if (len(clientRequest) == 3):
-	# 	proxySocket.connect((urllib.parse.urlparse(clientRequest[1]).netloc, 80))
-	# 	proxyRequest ='GET ' + urllib.parse.urlparse(clientRequest[1]).path + ' HTTP/1.0\r\nHost: ' + urllib.parse.urlparse(clientRequest[1]).netloc +'\r\n' +'Connection: close' + '\r\n\r\n'
-	# else:
-	# 	newstr = urllib.parse.urlparse(clientRequest[3]).path.replace('\r\n', '')
-	# 	proxySocket.connect((newstr, 80))
-	# 	proxyRequest ='GET ' + urllib.parse.urlparse(clientRequest[1]).path + ' HTTP/1.0\r\nHost: ' + newstr +'\r\n' +'Connection: close' + '\r\n\r\n'
-	
-	# # forward request to end server, get response back
-	# proxySocket.send(proxyRequest.encode())
-	# responseMessage = proxySocket.recv(1024)
-	# proxySocket.close()
-
-	# # send response back to client
-	# connectionSocket.send(responseMessage)
-	# connectionSocket.close()
-
 #main():
 #	print("hello")
 #if __name__ == "__main__":
